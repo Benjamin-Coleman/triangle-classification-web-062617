74c877b143f83bb76d8811aeed4b6a953a2da7ab
Benjamin-Coleman <github email address>
2017-07-02 18:23:12 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+	attr_accessor :side_lengths, :side1, :side2, :side3
+
+	def initialize(side1, side2, side3)
+		@side1 = side1
+		@side2 = side2
+		@side3 = side3
+		@side_lengths = [@side1, @side2, @side3].sort
+	end
+
+
+	def kind
+		if (@side_lengths[0] + @side_lengths[1] <= @side_lengths[2]) || @side_lengths[0] <= 0
+			begin
+				raise TriangleError
+			end
+		elsif (@side_lengths[0] == @side_lengths[1] && @side_lengths[0] == @side_lengths[2])
+			return :equilateral
+		elsif (@side_lengths[1] == @side_lengths[2] )
+			return :isosceles
+		else
+			return :scalene
+		end
+	end
+
 end
+
+class TriangleError < StandardError
+	def message
+		"Invalid triangle!"
+	end
+end
+
+eq = Triangle.new(5,5,5)
+sca = Triangle.new(3,10,5)
